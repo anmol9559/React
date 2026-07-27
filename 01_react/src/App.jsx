4a3bb10043e777faba456ba97011b5cb232e17ed
Anmol Soni <anmolsoni945@gmail.com>
2026-07-27 23:35:11 +0530

    tailwind install

--- 01_react/src/App.jsx
+++ 01_react/src/App.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div className="bg-blue-700 text-white p-4">
+      <h1 className='text-2xl font-bold'>Hello, World!</h1>
+      <p>This is a simple React application.</p>
+    </div>
+    )
 }
 
 export default App
