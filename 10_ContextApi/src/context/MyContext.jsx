359a28f752569d4517484de0fe31dc9d4c1db534
Anmol Soni <anmolsoni945@gmail.com>
2026-08-13 01:43:47 +0530

    changes in 10_ContexApi

--- 10_ContextApi/src/context/MyContext.jsx
+++ 10_ContextApi/src/context/MyContext.jsx
@@ -1,17 +1,17 @@
-import { createContext, useState } from "react";
+// import { createContext, useState } from "react";
 
-// setup out blank store---
-export const MyStore = createContext();
+// // setup out blank store---
+// export const MyStore = createContext();
 
-// make a provider of our store who handle data
-//  and serves to the customers
-export const ContextProvider = ({ children }) => {
-  const [centralValue, setCentralValue] = useState("me context se hu");
-  const [cartItems, setCartItems] = useState([]);
+// // make a provider of our store who handle data
+// //  and serves to the customers
+// export const ContextProvider = ({ children }) => {
+//   const [centralValue, setCentralValue] = useState("me context se hu");
+//   const [cartItems, setCartItems] = useState([]);
 
-  return (
-    <MyStore.Provider value={{ centralValue, cartItems, setCartItems }}>
-      {children}
-    </MyStore.Provider>
-  );
-};
+//   return (
+//     <MyStore.Provider value={{ centralValue, cartItems, setCartItems }}>
+//       {children}
+//     </MyStore.Provider>
+//   );
+// };
